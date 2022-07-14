8a47558965a9b7a0472b09ebbf2d81340c8d1a9b
Ivan Povazan <ivan.povazan@gmail.com>
2022-07-14 18:13:53 +0200

    Fixing the script

--- get-dotnet-pkg-url.sh
+++ get-dotnet-pkg-url.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+
+#Example: DOTNET_VERSION=7.0.100-preview.7.22354.2
+DOTNET_VERSION=
 if [ -z "$1" ]; then
-    echo "Pass the dotnet version you would like to get the download URL for"
+    echo "Pass the dotnet version you would like to get the download URL for example './get-dotnet-pkg-url.sh 7.0.100-preview.7.22354.2'"
     exit 1
 else
     DOTNET_VERSION=$1
@@ -19,8 +23,7 @@ CMD="$DOTNET_INSTALLER --version $DOTNET_VERSION --architecture arm64 --no-path 
 RES=$(eval "$CMD")
 for url in $RES;
 do
-    #pkg=${url%.tar.gz}.pkg
-    pkg=$url
+    pkg=${url%.tar.gz}.pkg
     if curl -LI --fail "$pkg" >/dev/null 2>&1; then echo "$pkg"; break; fi;
 done
 
